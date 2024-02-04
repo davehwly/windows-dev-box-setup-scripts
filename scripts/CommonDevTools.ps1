7607692c651c618facf8a575cb9b94b8cbf4271b
Dave <davidhawley46@gmail.com>
2024-02-04 17:13:31 +0000

    corrected spelling

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -5,6 +5,6 @@ choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTermi
 # choco install -y 7zip.install
 # choco install -y sysinternals
 # choco install -y agentransack
-# choco isntall -y sublimetext3.app
+choco install -y sublimetext3
 choco install -y beyondcompare
 choco install -y postman
