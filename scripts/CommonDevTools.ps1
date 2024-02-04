d684b773a3d6685f3d5920bb9765e2cd430220a9
Dave <davidhawley46@gmail.com>
2024-02-04 17:06:13 +0000

    remove problem packages

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,11 +1,10 @@
 
 # tools we expect devs across many scenarios will want
-choco install -y vscode
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
 # choco install -y python
 # choco install -y 7zip.install
 # choco install -y sysinternals
-choco install -y agentransack
-choco isntall -y sublimetext3.app
+# choco install -y agentransack
+# choco isntall -y sublimetext3.app
 choco install -y beyondcompare
 choco install -y postman
